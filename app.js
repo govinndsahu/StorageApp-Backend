68a55b590738f6ecf99959e727e0266cfb378699
GOVIND <work.govindsahu@gmail.com>
2025-12-12 19:18:03 +0530

    ready-for-render-deployment-test-5

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Origin",
     "https://storage-app-frontend.vercel.app"
   );
-
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   cors({
     origin: [
       "http://192.168.1.10:5173",
