dd6368b168b5fd23568406af6737867a520ce610
GOVIND <work.govindsahu@gmail.com>
2025-12-12 20:32:16 +0530

    ready-for-render-deployment-test-13

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
     origin: process.env.CLIENT_URL || "https://storage-app-frontend.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "Cookie", "filesize"],
+    allowedHeaders: ["Content-Type", "Authorization", "Cookie", "filesize", "filename"],
     exposedHeaders: ["Set-Cookie"],
     preflightContinue: false,
     optionsSuccessStatus: 204,
